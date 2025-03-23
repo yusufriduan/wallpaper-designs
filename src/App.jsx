ba92090a80fb5d5c079a59c8295be7b6fb3148cc
Yusuf Riduan <yusufriduan@outlook.com>
2025-03-23 13:48:46 +0800

    Main page (WIP)

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import favIcon from './static/star_favorite_3421.ico';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <h4>welcome to</h4>
+        <h2>wallpaper designs</h2>
+
+        <h6>why think about wallpaper designs,<br />when you can just slaps into template?</h6>
+        <div className='buttons'>
+          <button className='start'>Get started!</button>
+          <a className='favourite' href='https://www.github.com/yusufriduan/'>
+            <i class="fa fa-bookmark-o" style={{ fontSize: '25px' }}></i>
+          </a>
+        </div>
+        </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
